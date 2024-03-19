3fd31b456fc79ee9caa5e8c6f8161399797e449a
Kürşat Doğan Çelik <kursatdogancelik@gmail.com>
2024-03-19 17:42:57 +0300

    ReactDOM render method tried with jsx file

---
+++ JSX/src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+ReactDOM.render(
+  <div>
+    <h1>Heyyo World!</h1>
+    <p>This is fun ^~^</p>
+  </div>,
+  document.getElementById("root")
+)
+
