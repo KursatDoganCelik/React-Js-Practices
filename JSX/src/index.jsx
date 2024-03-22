bbd55fb5e55d84a10386f61aa8a14356c979ea30
Kürşat Doğan Çelik <kursatdogancelik@gmail.com>
2024-03-22 05:48:48 +0300

    some usage are applied

--- JSX/src/index.jsx
+++ JSX/src/index.jsx
@@ -1,10 +1,23 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+const greetingStart = "Heyyo"
+const greetingEnd = "World!"
+const randomImg  = "https://picsum.photos/200"
+
+const customStyle = {
+  display: "inline-block",
+  color: "blue",
+  fontSize: "30px",
+  border: "1px solid blue"
+}
+customStyle.color = "green"
+
 ReactDOM.render(
   <div>
-    <h1>Heyyo World!</h1>
+    <h1 className="bgcBlack" style={customStyle}>{`${greetingStart} ${greetingEnd}`}</h1>
     <p>This is fun ^~^</p>
+    <img src={randomImg} />
   </div>,
   document.getElementById("root")
 )
