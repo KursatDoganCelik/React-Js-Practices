54a5272318fc2d0d766823d129939a65a37f12a3
Kürşat Doğan Çelik <kursatdogancelik@gmail.com>
2024-03-23 18:00:00 +0300

    components added

---
+++ Component/src/components/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Pasta</li>
+      <li>Kebab</li>
+      <li>Sugar :P</li>
+    </ul>
+  );
+}
+
+export default List;
