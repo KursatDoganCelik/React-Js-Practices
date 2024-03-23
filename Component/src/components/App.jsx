822f56b2ed14f26f0a0ce28bd1d763d133ffd831
Kürşat Doğan Çelik <kursatdogancelik@gmail.com>
2024-03-23 18:32:50 +0300

    pi values used

--- Component/src/components/App.jsx
+++ Component/src/components/App.jsx
@@ -1,12 +1,16 @@
 import React from "react"
 import Heading from "./Heading"
 import List from "./List"
+import * as Pie from "./PiValues"
 
 function App() {
   return (
     <div>
       <Heading />
       <List />
+      <li>{Pie.default}</li>
+      <li>{Pie.doublePi}</li>
+      <li>{Pie.triplePi()}</li>
     </div>
   )
 }
