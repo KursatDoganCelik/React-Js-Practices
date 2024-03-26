33c7a2bdadb8e4bee157c8fc62af9702c177b2eb
Kürşat Doğan Çelik <kursatdogancelik@gmail.com>
2024-03-26 14:24:58 +0300

    component are added

---
+++ Conditional Render/src/components/Register.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Input from "./Input"
+import Button from "./Button"
+
+function Register() {
+  return(
+    <form className="form">
+      <Input type="text" placeholder="Username" />
+      <Input type="password" placeholder="Password" />
+      <Input type="password" placeholder="Confirm Password" />
+      <Button type="Submit" text="Register" />
+    </form>
+  )
+}
+
+export default Register
